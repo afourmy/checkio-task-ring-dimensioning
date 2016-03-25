7c04a06ed30944c4753447d8b24e860e63853617
mintoo <minto@hotmail.fr>
2016-03-25 21:28:29 +0200

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -10,21 +10,21 @@ Each test is dict with
 TESTS = {
     "Basics": [
         {
-            "input": ["AEFCBG", ("AC", 5), ("EC", 10), ("AB", 60)],
+            "input": ["AEFCBG", [("AC", 5), ("EC", 10), ("AB", 60)]],
             "answer": [2, 2, 1, 0, 0]
         },
         {
-            "input": ["ABCDEFGH", ("AD", 4)],
+            "input": ["ABCDEFGH", [("AD", 4)]],
             "answer": [0, 0, 3, 0, 0]
         },
         {
-            "input": ["ABCDEFGH", ("AD", 4), ("EA", 41)],
+            "input": ["ABCDEFGH", [("AD", 4), ("EA", 41)]],
             "answer": [4, 0, 3, 0, 0]
         }
     ],
     "Extra": [
         {
-            "input": ["ADBC", ("AD",1), ("DB", 9), ("BC", 0.345), ("CA", 13)],
+            "input": ["ADBC", [("AD",1), ("DB", 9), ("BC", 0.345), ("CA", 13)]],
             "answer": [0, 1, 1, 2, 0]
         }
     ]
